28cea2b1e1b4a00dd16d766dd3e5d17bcf7833a9
Aswanthachu <pnkaswanthachu546@gmail.com>
2023-03-10 07:31:17 +0530

    nextauth is added to project for google authentication

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,27 @@
 import SideBar from "../components/SideBar";
+import SessionProvider from "../components/SessionProvider";
+import { getServerSession } from "next-auth";
 import "../styles/globals.css";
+import { authOptions } from "../pages/api/auth/[...nextauth]";
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await getServerSession(authOptions);
   return (
     <html>
       <head />
       <body>
-        <div className="flex">
-          <SideBar />
+        <SessionProvider session={getServerSession}>
+          <div className="flex">
+            <SideBar />
 
-          {/* ClientProvider - Notification */}
-          <div className="flex-1 bg-[#343541] text-white">{children}</div>
-        </div>
+            {/* ClientProvider - Notification */}
+            <div className="flex-1 bg-[#343541] text-white">{children}</div>
+          </div>
+        </SessionProvider>
       </body>
     </html>
   );
